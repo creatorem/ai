c4bfae52bad0b86a543c5021781723c800e2ee7e
arnaudpfu <arnaudpfundstein.pro@gmail.com>
2026-02-04 11:16:56 +0100

    clean: remove the nextjs-example_ app

--- apps/nextjs-example_/eslint.config.mjs
+++
@@ -1,4 +0,0 @@
-import eslintConfigApps from '@tooling/eslint-config/apps.js';
-import eslintConfigBase from '@tooling/eslint-config/base.js';
-
-export default [...eslintConfigBase, ...eslintConfigApps];
--- apps/nextjs-example_/postcss.config.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from '@kit/tailwind-config/postcss.config.js';
